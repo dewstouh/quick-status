9f5d0989b412c379b84cd0d145005d4dd362c876
dewstouh <diego@justdiego.com>
2025-07-09 15:00:10 +0200

    feat(core): add outage service to create,get,end,get actives,get history services

---
+++ packages/core/src/services/outage/index.ts
@@ -0,0 +1,45 @@
+import { OutageType, Prisma } from '@prisma/client';
+import prisma from "@quick-status/db";
+
+export default class OutageService {
+    static async create(siteId: number, type: OutageType) {
+        return prisma.outage.create({
+            data: {
+                siteId,
+                type
+            }
+        })
+    }
+
+    static async get(id: number) {
+        return prisma.outage.findUnique({
+            where: { id }
+        })
+    }
+
+    static async end(id: number) {
+        return prisma.outage.update({
+            where: { id },
+            data: { endTime: new Date() }
+        })
+    }
+
+    static async getActiveBySite(siteId: number) {
+        return prisma.outage.findFirst({
+            where: {
+                siteId,
+                endTime: null
+            }
+        });
+    }
+
+    static async getHistory(siteId: number, since: Date) {
+        return prisma.outage.findMany({
+            where: {
+                siteId,
+                startTime: { gte: since }
+            },
+            orderBy: { startTime: 'desc' }
+        });
+    }
+}   
