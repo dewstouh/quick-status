f36117e028193546bb6a96830607f30084b23dcd
dewstouh <diego@justdiego.com>
2025-07-09 13:19:11 +0200

    refactor: replace @repo with @quick-status

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@quick-status/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
