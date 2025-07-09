a019f3e6aafd01c2542cb55dc76213c232445184
dewstouh <diego@justdiego.com>
2025-07-09 14:45:46 +0200

    feat(core): add SiteService to manage sites

---
+++ packages/core/src/services/site/index.ts
@@ -0,0 +1,47 @@
+import prisma from "@quick-status/db";
+
+export default class SiteService {
+    static async create(name:string, url:string){
+        return prisma.site.create({
+            data: {
+                name,
+                url
+            }
+        })
+    }
+
+    static async getById(id:number) {
+        return prisma.site.findUnique({
+            where: { id }
+        })
+    }
+
+    static async getByName(name:string) {
+        return prisma.site.findUnique({
+            where: { name }
+        })
+    }
+
+    static async getByURL(url:string) {
+        return prisma.site.findUnique({
+            where: { url }
+        })
+    }
+
+    static async getAll() {
+        return prisma.site.findMany();
+    }
+
+    static async update(id:number, data:Partial<{name:string, url:string}>) {
+        return prisma.site.update({
+            where: { id },
+            data
+        })
+    }
+
+    static async delete(id:number) {
+        return prisma.site.delete({
+            where: { id }
+        })
+    }
+}   
